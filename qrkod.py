1df34cbc79204ce42df1f3d85b9469e1a7a04bdc
hamer1818 <47240233+hamer1818@users.noreply.github.com>
2021-03-24 10:17:08 +0300

    Add files via upload

---
+++ qrkod.py
@@ -0,0 +1,27 @@
+import urllib.request
+import urllib.parse
+
+try:
+    print("\n")
+
+    size=input("Karekod boyutunu giriniz:")
+
+    veri=input("İçerik")
+
+    veriler= {
+        'size' :size+ 'x'+ size,
+        'data' :veri
+    }
+
+    parametreler=urllib.parse.urlencode(veriler)
+
+    api_link="https://api.qrserver.com/v1/create-qr-code/?"+parametreler
+
+    urllib.request.urlretrieve(api_link,'kare_kod.png')
+    print("\n")
+
+    print("Karekod başarıyla oluşturuldu.")
+    print("kare_kod.png isimli dosyadan ulaşabilirsiniz.")
+    print("\n")
+except:
+    print("Beklenmeyen bir hata oluştu.")
